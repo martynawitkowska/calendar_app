ea8dbb68dabe38a580d650bf36cacb8d019e4e64
Martyna Witkowska <mar.witkowska@gmail.com>
2022-11-14 21:56:43 +0100

    function for generating objects in main

--- main.py
+++ main.py
@@ -1,14 +1,3 @@
-from datetime import datetime
-from pprint import pprint
+from helpers import generate_objects
 
-from data_generator import DataGenerator
-from event import Event
-
-events = DataGenerator.load_data('event_data.json')
-events_objects = []
-
-for event in events:
-    event['start_date'] = datetime.strptime(event['start_date'], '%Y/%m/%d, %H:%M')
-    events_objects.append(Event(**event))
-
-pprint(events_objects)
+data = generate_objects()
