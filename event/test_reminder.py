c5e2e0bd01197cabb50e8112a40d31f25cab29d2
Martyna Witkowska <mar.witkowska@gmail.com>
2022-12-29 20:35:07 +0100

    change test creation object of type Reminder

--- event/test_reminder.py
+++ event/test_reminder.py
@@ -1,23 +1,9 @@
 from datetime import datetime, timedelta
 
-import pytest
-
 from event import Reminder
 
-@pytest.fixture
-def reminder():
-    return Reminder(1, datetime.now() + timedelta(hours=3), 20, '', '', '', True)
 
+def test_create_reminder_instance():
+    reminder = Reminder(1, datetime.now() + timedelta(hours=3), 20, '', '', '', True)
 
-def test_duration_less_than_ten_minutes_raise_value_error():
-    with pytest.raises(ValueError) as excinfo:
-        r = Reminder(1, datetime.now() + timedelta(hours=3), 5, '', '', '', True)
-
-        assert 'can not be shorter than 10 minutes' in str(excinfo.value)
-
-
-def test_duration_change_to_less_than_ten_minutes_raise_value_error(reminder):
-    with pytest.raises(ValueError) as excinfo:
-        reminder.duration = 5
-
-        assert 'can not be shorter than 10 minutes' in str(excinfo.value)
+    assert reminder.remind == True
